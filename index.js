44e87189729ab4c42bdb1a39df962459b7fda715
luqman <luqmanito602@gmail.com>
2023-10-17 07:47:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
+var num1 = 31
+var num2 = 2
+
+var multiply = num1 * num2;
+
+if (multiply === 62) {
+    console.log("The multiplication is 62.");
+} else {
+    console.log("The multiplication is not 62.");
+}
+
+var random = Math.ceil(Math.random() * 1000);
+
+console.log("Random Integer Greater Than 0: " + random);
+
+var num3 = 14;
+var num4 = 10;
+
+var mod = num3 % num4;
+
+if (mod === 4) {
+    console.log("The remainder is 4.");
+} else {
+    console.log("The remainder is not 4.");
+}
+
+
+var numbers = [10, 5, 20, 15, 8];
+
+var max = Math.max(...numbers);
+
+if (max === 20) {
+    console.log("The highest number in the set is 20.");
+} else {
+    console.log("The highest number in the set is not 20.");
+}
 
 // Write your code here
